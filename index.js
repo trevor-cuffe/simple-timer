c402a0eb0cd8fd759f3de957b015b5d9e9e2d665
trevor-cuffe <trevor.d.cuffe@gmail.com>
2020-12-15 10:05:43 -0500

    Add color change fading effect

--- index.js
+++ index.js
@@ -59,13 +59,42 @@ function setStartButtonIcon(isRunning) {
 }
 
 function setTimerColor(progress) {
-    //apply color-change at certain intervals:
-    if (progress >= .5) {
-        timerCircle.style.stroke = "green";
-    } else if (progress >= .25) {
-        timerCircle.style.stroke = "yellow";
-    } else {
-        timerCircle.style.stroke = "red";
-    }
+    // fade color-change through yellow and red
+    // Green until 70%
+    // Yellow at 40%
+    // Red at 10%
+
+    // green  = rgb(  0, 128, 0)
+    // yellow = rgb(255, 255, 0)
+    // red    = rgb(255,   0, 0)
+
+    // RED CALCULATION:
+    // starting at 70%, add red over 30% of the duration (1/3.33) 
+    let red =
+        0
+        + 255 * Math.min(1, (progress < 0.7) * (0.7 - progress) * 3.33);
+    
+    // GREEN CALCULATION:
+    // starting at 70%, increase to 255 over 30% of the duration (1/3.33)
+    // starting at 40%, reduce to 0 over 30% of the duration (1/3.33)
+    let green =
+        128
+        + 127 * Math.min(1, (progress < 0.7) * (0.7 - progress) * 3.33)
+        - 255 * Math.min(1, (progress < 0.4 ) * (0.4 - progress) * 3.33);
+
+    timerCircle.style.stroke = `rgb(${red}, ${green}, 0)`;
+   
+   
+    // //apply color-change at certain intervals:
+    // if (progress >= .5) {
+    //     timerCircle.style.stroke = "green";
+    // } else if (progress >= .25) {
+    //     timerCircle.style.stroke = "yellow";
+    // } else {
+    //     timerCircle.style.stroke = "red";
+    // }
 }
 
+
+//Next Step:
+//Allow user to escape timer input and return to previous value
